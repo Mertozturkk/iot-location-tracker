d266eee6a1aea5f6d490144c438aae3f308b9164
Mert Öztürk <ozturkmmertt@gmail.com>
2024-05-31 19:12:06 +0300

    changed parameters

--- src/app/utils/consumer.py
+++ src/app/utils/consumer.py
@@ -4,10 +4,19 @@ from src.app.database.models import Base, Location, Device
 from datetime import datetime
 from src.app.database.session import SessionLocal
 from src.app.utils.validator import validate_and_prepare_data, project_logger
+from src.app.config.config import settings
 
 
 @validate_and_prepare_data
 def save_data_to_db(data):
+
+    """
+        Save device and location data to the database.
+
+        Args:
+            data (dict): The data to be saved, containing device and location information.
+        """
+
     session = SessionLocal()
     try:
         device = Device(id=data['device_id'], name=data['device_name'], serial_number=data['serial_number'],
@@ -37,15 +46,29 @@ def save_data_to_db(data):
 
 
 def callback(ch, method, properties, body):
+    """
+        Callback function to process received messages.
+
+        Args:
+            ch: The channel object.
+            method: The method frame with delivery information.
+            properties: The properties of the message.
+            body (bytes): The body of the message.
+        """
+
     location_data = json.loads(body)
     project_logger.info(f"Received data: {location_data}")
     save_data_to_db(location_data)
 
 
 def consumer():
+    """
+        Set up the consumer to receive and process messages from the queue.
+    """
+
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='iot_queue')
-    channel.basic_consume(queue='iot_queue', on_message_callback=callback, auto_ack=True)
+    channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
+    channel.basic_consume(queue=settings.RABBITMQ_QUEUE, on_message_callback=callback, auto_ack=True)
     project_logger.debug('Waiting for messages...')
     channel.start_consuming()
--- src/app/utils/queue.py
+++ src/app/utils/queue.py
@@ -1,10 +1,11 @@
 import pika
 import json
 from src.app.utils.validator import project_logger
+from src.app.config.config import settings
 
 
 def get_connection():
-    credentials = pika.PlainCredentials('guest', 'guest')
+    credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD)
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost', credentials=credentials)
     )
@@ -14,11 +15,11 @@ def get_connection():
 def enqueue_data(data):
     connection = get_connection()
     channel = connection.channel()
-    channel.queue_declare(queue='iot_queue')
+    channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
 
     message = json.dumps(data)
     project_logger.debug(f"Enqueueing data: {message}")
     channel.basic_publish(exchange='',
-                          routing_key='iot_queue',
+                          routing_key=settings.RABBITMQ_QUEUE,
                           body=message)
     connection.close()
